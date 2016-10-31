c7a11d034602c533d9d299a9b5ccd8fdb82551b0
sundusk <zhuyue346@gmail.com>
2016-10-31 16:45:37 +0800

    项目创建

---
+++ DazzleLight/DazzleLight/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DazzleLight
+//
+//  Created by 夜兔神威 on 2016/10/31.
+//  Copyright © 2016年 ccq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
