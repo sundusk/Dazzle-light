8c0ce375c679b189a1bc3b8d0b4e2c4aa1be35ad
caichangqing <caichangqing@bingdou.tv>
2017-01-20 12:36:36 +0800

    实现总入口的跳转

--- DazzleLight/DazzleLight/CCQMainTableViewController.m
+++ DazzleLight/DazzleLight/CCQMainTableViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "CCQMainTableViewController.h"
-
+static NSString *cellID = @"cellID";
 @interface CCQMainTableViewController ()
-
+@property (nonatomic, strong) NSMutableArray *titles;
+@property (nonatomic, strong) NSMutableArray *classNames;
 @end
 
 @implementation CCQMainTableViewController
@@ -17,10 +18,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.navigationController.title = @"程序总入口";
+    
+    
+    
+    self.titles  = [NSMutableArray array];
+    self.classNames = [NSMutableArray array];
+    [self addCell:@"用户信息反馈" class:@"CCQUserFeedBackViewController"];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellID];
+    [self.tableView reloadData];
     
 }
-
+- (void)addCell:(NSString *)title class:(NSString *)className {
+    [self.titles addObject:title];
+    [self.classNames addObject:className];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
@@ -35,20 +48,25 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 
-    return 10;
+    return self.titles.count;
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    NSInteger section = indexPath.section;
-    NSInteger row = indexPath.row;
     
-    UITableViewCell *cell = [[UITableViewCell alloc]init];
     
-    //给cell里的文本标签设置文字内容
-    cell.textLabel.text = [NSString stringWithFormat:@"我是第%zd组第%zd行的数据",section,row];
-    
-    //    cell.backgroundColor = [UIColor grayColor];
-    //    cell.backgroundColor = [UIColor clearColor];
-    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID forIndexPath:indexPath];
+    cell.textLabel.text = _titles[indexPath.row];
+   
     return cell;
+}
+// 点击cell跳转控制器
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *className = self.classNames[indexPath.row];
+    Class class = NSClassFromString(className);
+    if (class) {
+        UIViewController *ctrl = class.new;
+        ctrl.title = _titles[indexPath.row];
+        [self.navigationController pushViewController:ctrl animated:YES];
+    }
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 @end
